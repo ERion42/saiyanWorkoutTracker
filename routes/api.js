6c85eb6cd9e3dd30ffb0dc54461d44c0eb22eed9
Eric Rion <erion42@gmail.com>
2021-11-14 16:13:10 -0500

    Blanked out routes to start over

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,8 @@
+const path = require("path");
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
-const Transaction = require("../models/workout.js");
 
 // routes stuff goes here
-router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-    .sort({ date: -1 })
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-    .catch((err) => {
-        res.status(400).json(err);
-    });
-});
 
 // 
 
